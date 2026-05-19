8d7272e5358038784099ea9191b9f8803b557ef6
Muhammad Anus <mohdanus20@gmail.com>
2026-05-19 14:57:36 +0500

    .

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   typedRoutes: true,
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
